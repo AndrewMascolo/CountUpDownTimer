fbd2f13e8e4dc5697db38ee9c0db153a5db04708
AndrewMascolo <Adeathsk8r@comcast.net>
2016-06-24 18:49:19 -0400

    Update CountUpDownTimer.h
    Added precision to the library, should help to allow for longer durations. Insert either LOW or HIGH as the second condition in the constructor to select your precision mode. LOW = millis() and HIGH = micros(). Default is HIGH

--- CountUpDownTimer.h
+++ CountUpDownTimer.h
@@ -39,14 +39,18 @@ SOFTWARE.
 class CountUpDownTimer
 {
   public:
-    CountUpDownTimer(bool type) : _type(type)
+        CountUpDownTimer(bool type, bool precision = HIGH) : _type(type), _precision(precision)
 	{ 
 	  SetStopTime((type? 0xFFFF : 0)); // 18h 12m 15s
-	  time = micros();
+	  time = precision ? micros() : millis();
 	  Clock = 0;
 	  Reset = false, Stop = true, Paused = true;
 	  timeFlag = false;
-	  duration = 1000000;
+	  duration = precision ? 1000000 : 1000;
+	}
+	unsigned long _InternalClock()
+	{
+	  return _precision ? micros() : millis();
 	}
 	
 	boolean Timer()
@@ -55,9 +59,9 @@ class CountUpDownTimer
 	  if (!Stop && !Paused) // if not Stopped or Paused, run timer
 	  {
 	    if(Paused)
-	      time = micros();
+	      time = _InternalClock();
 		
-		if ((_micro = micros()) - time > duration ) // check the time difference and see if 1 second has elapsed
+		if ((_intTime = _InternalClock() ) - time > duration ) // check the time difference and see if 1 second has elapsed
 		{
 		  _type == UP? Clock++ : Clock--;
 			
@@ -66,9 +70,9 @@ class CountUpDownTimer
 		  if ((_type == DOWN && Clock == 0) || TimeCheck()) // check to see if the clock is 0
 			Stop = true; // If so, stop the timer
 			
-		  time = _micro;
+		  time = _intTime;
 		  
-		  if(_micro < time) 
+		  if(_intTime < time) 
 		    time = 0;  // check to see if micros() has rolled over, if not, then increment "time" by duration
 		}
 	  }
@@ -87,7 +91,7 @@ class CountUpDownTimer
 
 	void StartTimer()
 	{
-	  Watch = micros();
+	  Watch = _InternalClock();
 	  Stop = false;
 	  Paused = false;
 	  if(_type == UP) 
@@ -107,14 +111,14 @@ class CountUpDownTimer
 
 	void PauseTimer()
 	{
-	  time = micros();
+	  time = _InternalClock();
 	  Paused = true;
 	}
 
 	void ResumeTimer() // You can resume the timer if you ever stop it.
 	{
 	  Paused = false;
-	  time = micros();
+	  time = _InternalClock();
 	}
 
 	void SetTimer(unsigned long hours, unsigned long minutes, unsigned long seconds)
@@ -147,7 +151,7 @@ class CountUpDownTimer
 	
 	void SetStopTime(unsigned long hours, unsigned long minutes, unsigned long seconds)
 	{
-      STh = hours;
+          STh = hours;
 	  STm = minutes;
 	  STs = seconds;
 	  STotaltime = (hours * 3600) + (minutes * 60) + (seconds % 60);
@@ -170,12 +174,18 @@ class CountUpDownTimer
 
 	unsigned long ShowMilliSeconds()
 	{
-	  return ((_micro - Watch)/ 1000.0) + 1;
+	  if (_precision == HIGH)
+	    return ((_intTime - Watch)/ 1000.0) + 1;
+	  else
+	    return (_intTime - Watch) + 1;
 	}
 
 	unsigned long ShowMicroSeconds()
 	{
-	  return (_micro - Watch) + 1;
+	  if (_precision == LOW)
+	    return ((_intTime - Watch)/ 1000.0) + 1;
+	  else
+	    return (_intTime - Watch) + 1;
 	}
 	
 	unsigned long ShowTotalSeconds()
@@ -199,7 +209,7 @@ class CountUpDownTimer
 	  if(_type)
 	    return (Clock >= TC);
 	  else  
-	    return (Clock <= TC );
+	    return (Clock <= TC);
 	}
 	
 	boolean TimeCheck() // output true if timer equals requested time or has passed it.
@@ -210,20 +220,14 @@ class CountUpDownTimer
 	    return Clock <= STotaltime;  
 	}
 	
-	boolean ExactTimeCheck(unsigned int hours, unsigned int minutes, unsigned int seconds) // output true if timer equals the exact requested time.
-	{
-	  unsigned long TC = ((hours * 3600) + (minutes * 60) + (seconds % 60));
-	  return Clock == TC;
-	}
-	
     private:
 	    unsigned long duration;
 	    unsigned long STh, STm, STs, STotaltime;
-	    unsigned long Watch, _micro, time;
-	    unsigned long Clock, R_clock;
+	    unsigned long Watch, _intTime, time;
+            unsigned long Clock, R_clock;
 	    boolean Reset, Stop, Paused;
 	    volatile boolean timeFlag;
-	    bool _type;
+	    bool _type, _precision;
 };
 
 #endif
